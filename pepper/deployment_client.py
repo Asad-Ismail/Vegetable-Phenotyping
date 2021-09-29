94008d995ce224c34876400acc277374ee07af44
Asad Ismail <asadismaeel@gmail.com>
2021-09-29 19:12:43 +0200

    Added Inference using FASTAPI

---
+++ pepper/deployment_client.py
@@ -0,0 +1,62 @@
+import os
+import io
+import cv2
+import requests
+import numpy as np
+import json
+
+
+base_url = 'http://localhost:8000'
+endpoint = '/predict'
+url_with_endpoint_no_params = base_url + endpoint
+
+# If we need to pass parameter
+#full_url = url_with_endpoint_no_params + "?model=" + model
+#full_url
+
+def response_from_server(url, image_file, verbose=True):
+    """Makes a POST request to the server and returns the response.
+
+    Args:
+        url (str): URL that the request is sent to.
+        image_file (_io.BufferedReader): File to upload, should be an image.
+        verbose (bool): True if the status of the response should be printed. False otherwise.
+
+    Returns:
+        requests.models.Response: Response from the server.
+    """
+    
+    files = {'file': image_file}
+    response = requests.post(url, files=files)
+    status_code = response.status_code
+    if verbose:
+        msg = "Everything went well!" if status_code == 200 else "There was an error when handling the request."
+        print(msg)
+    return response
+
+def display_image_from_response(response):
+    """Display image within server's response.
+
+    Args:
+        response (requests.models.Response): The response from the server after object detection.
+    """
+    #print(f"Response is {response.content}")
+    out=json.loads(response.content.decode('utf-8'))
+    #print(f"The shape of Response Image is {out['Detected']}")
+    detected=np.array(out['Detected'],dtype=np.uint8)
+    cv2.imshow("Output",detected)
+    cv2.waitKey(0)
+    #image_stream = io.BytesIO(response.content)
+    #image_stream.seek(0)
+    #file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
+    #image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    #filename = "image_with_objects.jpeg"
+    #cv2.imwrite(f'images_predicted/{filename}', image)
+
+
+image_path="images/test.jpg"
+
+with open(image_path, "rb") as image_file:
+    prediction = response_from_server(url_with_endpoint_no_params, image_file)
+
+display_image_from_response(prediction)
---
+++ pepper/deployment_server.py
@@ -0,0 +1,78 @@
+from pepper_segment_orient_pred import *
+import io
+import uvicorn
+import numpy as np
+import nest_asyncio
+from enum import Enum
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from fastapi.responses import StreamingResponse
+
+
+
+print(f"Intializing Model!!")
+model_path="/media/asad/ADAS_CV/vegs_results/models/pepper/keypoints/model_final.pth"
+pepp=detectroninference(model_path)
+print(f"Intializing Model Done!!")
+
+app = FastAPI(title='Deploying Pepper Phenotyping with FastAPI')
+
+
+# By using @app.get("/") you are allowing the GET method to work for the / endpoint.
+@app.get("/")
+def home():
+    return "Congratulations! Your API is working as expected. Now head over to http://localhost:8000/docs."
+
+
+# This endpoint handles all the logic necessary for the object detection to work.
+# It requires the desired model and the image in which to perform object detection.
+@app.post("/predict") 
+def prediction(file: UploadFile = File(...)):
+
+    print(f"Performing Prediction!!")
+    # 1. VALIDATE INPUT FILE
+    filename = file.filename
+    fileExtension = filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    if not fileExtension:
+        raise HTTPException(status_code=415, detail="Unsupported file provided.")
+
+    # 2. TRANSFORM RAW IMAGE INTO CV2 image
+    
+    # Read image as a stream of bytes
+    image_stream = io.BytesIO(file.file.read())
+    
+    # Start the stream from the beginning (position zero)
+    image_stream.seek(0)
+    
+    # Write the stream of bytes into a numpy array
+    file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
+    
+    # Decode the numpy array as an image
+    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    
+    # 3. RUN Pepper and keypoint detection
+    print(f"Running the prediction on Image!!")
+    detected_cucumber,all_masks,all_patches,boxes,keypoints,*_=pepp.pred(image)
+    # Add more results in dictionary as required
+    results={}
+    results["Detected"]=detected_cucumber.tolist()
+    
+    # Save it in a folder within the server
+    #cv2.imwrite(f'images_uploaded/{filename}', detected_cucumber)
+    
+    # 4. STREAM THE RESPONSE BACK TO THE CLIENT
+    
+    # Open the saved image for reading in binary mode
+    #file_image = open(f'images_uploaded/{filename}', mode="rb")
+
+    # Return the results as json encodded
+    json_compatible_item_data = jsonable_encoder(results)
+    return JSONResponse(content=json_compatible_item_data)
+    # Return the image as a stream specifying media type
+    #return StreamingResponse(file_image, media_type="image/jpeg")
+
+
+nest_asyncio.apply()
+host =  "127.0.0.1"
+uvicorn.run(app, host=host, port=8000)

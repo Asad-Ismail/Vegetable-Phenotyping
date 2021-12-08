7c0e0a9c4f44b184497f048e48e6ae6195fdc82a
Asad Ismail <asadismaeel@gmail.com>
2021-12-08 12:46:56 +0100

    Updated Image removal

--- utils/remove_files_IA.py
+++ utils/remove_files_IA.py
@@ -8,8 +8,8 @@ Scale_Cam=True
 write_new=True
 
 #src = "/media/asad/8800F79D00F79104/aws_bayer_Data/VIP_pepper_filter/VIP_pepper"
-src="/media/asad/8800F79D00F79104/aws_bayer_Data/VIP_pepper_filter/Pepper_ScaleCam"
-dst="/media/asad/8800F79D00F79104/aws_bayer_Data/VIP_pepper_filter/Scalecam_validate"
+src="/media/asad/8800F79D00F79104/pepper_2021/aws_data"
+dst="/media/asad/8800F79D00F79104/pepper_2021/aws_filtered_data"
 skip_names = ["100.jpg", "320.jpg", "608.jpg", "416.jpg", "608__4chan"]
 index = 0
 
@@ -20,6 +20,9 @@ for subdir, dirs, files in tqdm(os.walk(src)):
     for file in files:
         is_skipped = False
         img=cv2.imread((os.path.join(subdir, file)),-1)
+        if img is None:
+            print(f"{file} is not an image")
+            continue
         #Skip depth channel image
         if img.shape[2]==4 and not Scale_Cam:
             is_skipped=True
